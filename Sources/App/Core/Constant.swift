92542fbdb40c065b666e40fa31f2d8fe26c66bf5
Tuğcan ÖNBAŞ <tugcanonbas@hotmail.com>
2023-04-16 06:58:55 +0300

    mysql and postgresql default port added

--- Sources/App/Core/Constant.swift
+++ Sources/App/Core/Constant.swift
@@ -16,7 +16,7 @@ enum Constant {
 
     enum Database {
         static let hostname: String = Environment.get("DATABASE_HOST") ?? "localhost"
-        static let port: Int = Environment.get("DATABASE_PORT").flatMap(Int.init) ?? 5432
+        static let port: Int = Environment.get("DATABASE_PORT").flatMap(Int.init) ?? {{#fluent.is_postgres}}5432{{/fluent.is_postgres}}{{#fluent.is_mysql}}3306{{/fluent.is_mysql}}
         static let username: String = Environment.get("DATABASE_USERNAME") ?? "vapor_username"
         static let password: String = Environment.get("DATABASE_PASSWORD") ?? "vapor_password"
         static let database: String = Environment.get("DATABASE_NAME") ?? "vapor_database"
